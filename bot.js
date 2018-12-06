c399796655a851645f4dd8fc652a27bea38066e6
nedal235 <38614159+nedal235@users.noreply.github.com>
2018-12-06 17:46:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,5 @@
 const Discord = require("discord.js");
 const prefix = ("!")
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-
-  bot.user.setGame("Dont even think to break roles", "https://www.twitch.tv/ninja");
-
-});
 
 client.on('message', function(message) {
     if(message.content.startsWith(prefix + "report")) {
